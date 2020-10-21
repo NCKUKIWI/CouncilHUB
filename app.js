c6e6e588b3b0f05806683d291f04dd3ec88f0e53
unknown <khjhs100266@gmail.com>
2020-10-21 16:24:57 +0800

    Added query to socketio

--- app.js
+++ app.js
@@ -28,26 +28,52 @@ const server = app.listen(3000, function () {
 const io = Server(server);
 
 io.on('connection', (socket) => {
-    // 去資料庫搜尋投票是否開始
-    if (isVoting === true) {
-        io.emit('start vote', "開始投票囉"); // 告訴大家
-    } else if (isVoting === false) {
-        io.emit('close vote', "開始投票囉"); // 告訴大家
-    }
+
+    socket.on('entry vote', (msg) => { //點進議案觸發
+        var proposalID = msg.proposalID;
+        var insertSql = "SELECT isVoting FROM `proposal` WHERE proposalID = " + proposalID;
+        db.Query(insertSql, function (voteInfo, err) {
+            var isVoting = voteInfo[0]["isVoting"];
+            if (isVoting === 0) {
+                io.emit('close vote', "投票結束囉"); // 告訴大家
+            } else if (isVoting === 1) {
+                io.emit('close vote', "開始決議案投票"); // 告訴大家
+            } else if (isVoting === 2) {
+                io.emit('close vote', "開始修正動議投票"); // 告訴大家
+            }
+        })
+    });
+
 
     socket.on('start vote', (msg) => { //議長觸發
-        io.broadcast.emit('start vote', "開始投票囉"); // 告訴大家
+        var proposalID = msg.proposalID;
+        var updateSql = "UPDATE `proposal` SET isVoting = 1 WHERE proposalID = " + proposalID;
+        db.Query(updateSql, function (updateResult, err) {
+            io.broadcast.emit('start vote', "開始決議案投票"); // 告訴大家
+        })
     });
     socket.on('close vote', (msg) => { //議長觸發
-        io.broadcast.emit('close vote', "沒投票囉"); // 告訴大家
+        var proposalID = msg.proposalID;
+        var updateSql = "UPDATE `proposal` SET isVoting = 0 WHERE proposalID = " + proposalID;
+        db.Query(updateSql, function (updateResult, err) {
+            io.broadcast.emit('close vote', "投票結束囉"); // 告訴大家
+        })
     });
 
 
-    socket.on('要投臨時動議囉', (msg) => {
-        io.broadcast.emit('臨時動議同意案');
+    socket.on('要投修正動議囉', (msg) => {
+        var proposalID = msg.proposalID;
+        var updateSql = "UPDATE `proposal` SET isVoting = 2 WHERE proposalID = " + proposalID;
+        db.Query(updateSql, function (updateResult, err) {
+            io.broadcast.emit('開始修正動議投票');
+        })
     });
-    socket.on('要關臨時動議囉', (msg) => {
-        io.broadcast.emit('同意不同意關掉');
-        // 如果同意>反對 議長去改資料
+    socket.on('要關修正動議囉', (msg) => {
+        var proposalID = msg.proposalID;
+        var updateSql = "UPDATE `proposal` SET isVoting = 0 WHERE proposalID = " + proposalID;
+        db.Query(updateSql, function (updateResult, err) {
+            io.broadcast.emit('close vote', "投票結束囉"); // 同意不同意關掉
+        })
+        // 如果同意>反對 議長自己去改資料XD
     });
 });
