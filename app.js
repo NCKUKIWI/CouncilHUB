63cba9280faee420a72bf610b473725a899afd8f
unknown <khjhs100266@gmail.com>
2020-11-16 00:15:20 +0800

    fixed app.js require bug

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const socket = require('socket.io')
-const db = require('../models/db')
+const db = require('./models/db')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
